526280526964795fe8a4040b6fdbccb6449a25eb
workanator <workanator@gmail.com>
2018-06-01 18:37:28 +0500

    Change Event fields types

--- event/event.go
+++ event/event.go
@@ -3,29 +3,61 @@ package event
 import "time"
 
 type Event struct {
-	Source   EventSource
-	Category EventCategory
-	Name     string
-	Time     time.Time
-	Data     interface{}
+	Source   EventSource   `json:"-"`
+	Category EventCategory `json:"category"`
+	Name     string        `json:"name"`
+	Time     time.Time     `json:"time"`
+	Data     interface{}   `json:"data,omitempty"`
 }
 
 // Event sources
 const (
-	_ EventSource = iota
-	SourceServer
-	SourceClient
-	SourceCustom
+	SourceServer EventSource = "server"
+	SourceClient EventSource = "client"
 )
 
-type EventSource uint8
+type EventSource string
+
+// Test if the event source is server
+func (es EventSource) IsServer() bool {
+	return es == SourceServer
+}
+
+// Test if the event source is client
+func (es EventSource) IsClient() bool {
+	return es == SourceClient
+}
+
+// Test if the event source is custom
+func (es EventSource) IsCustom() bool {
+	return es != SourceServer && es != SourceClient
+}
 
 // Event categories
 const (
-	_ EventCategory = iota
-	CategoryData
-	CategoryEvent
-	CategoryCustom
+	CategorySystem EventCategory = "system"
+	CategoryModel  EventCategory = "model"
+	CategoryEvent  EventCategory = "event"
 )
 
-type EventCategory uint8
+type EventCategory string
+
+// Test if the event category is system
+func (ec EventCategory) IsSystem() bool {
+	return ec == CategorySystem
+}
+
+// Test if the event category is model
+func (ec EventCategory) IsModel() bool {
+	return ec == CategoryModel
+}
+
+// Test if the event category is event
+func (ec EventCategory) IsEvent() bool {
+	return ec == CategoryEvent
+}
+
+// Test if the event category is custom
+func (ec EventCategory) IsCustom() bool {
+	return ec != CategorySystem && ec != CategoryModel && ec != CategoryEvent
+}
