ee75c6f63ac02c30efc9291fc0b2e89a11bc9737
workanator <workanator@gmail.com>
2018-06-04 18:20:03 +0500

    Embed RWLock for safe concurrent approach

--- facade/file_system.go
+++ facade/file_system.go
@@ -1,16 +1,24 @@
 package facade
 
-import "net/http"
+import (
+	"net/http"
+	"sync"
+)
 
 // FileSystem allows for attach multiple file systems to application. File systems are being searched for file
-// in the order they are attached.
+// in the order they are attached. FileSystems embeds sync.RWLock for protection from possible data races.
 type FileSystem struct {
+	sync.RWMutex
+
 	attached []http.FileSystem
 }
 
 // Attach file system fs.
 func (fileSys *FileSystem) Attach(fs http.FileSystem) {
 	if fs != nil {
+		fileSys.Lock()
+		defer fileSys.Unlock()
+
 		// Create the slice if nit and add the file system
 		if fileSys.attached == nil {
 			fileSys.attached = make([]http.FileSystem, 0, 1)
@@ -23,6 +31,9 @@ func (fileSys *FileSystem) Attach(fs http.FileSystem) {
 // Implement http.FileSystem interface. Open searches for the file with name on all file systems
 // attached in order of they were added.
 func (fileSys *FileSystem) Open(name string) (http.File, error) {
+	fileSys.RLock()
+	defer fileSys.RUnlock()
+
 	// Try to open file on attached file systems and return the first successful
 	for _, fs := range fileSys.attached {
 		if f, err := fs.Open(name); err != nil || f != nil {
